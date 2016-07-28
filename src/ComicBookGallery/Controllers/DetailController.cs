7dbd9cb7f8a09395f46e143adc5ea674b369f4c8
vjfloyd <vjrojasb@gmail.com>
2016-07-28 14:16:14 -0500

    add controller

---
+++ src/ComicBookGallery/Controllers/DetailController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string hola()
+        {
+            return "hi bitches";
+        }
+    }
+}
